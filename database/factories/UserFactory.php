b94d9d3da2f2394a0a9552b3c40a679fbf5997e9
Gomes84 <programadortrader12@gmail.com>
2024-04-17 10:32:14 +0100

    acesso

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'password' => static::$password ??= Hash::make('12345678'),
             'remember_token' => Str::random(10),
             'bi'=> fake()->name(),
-            'it_tipo_utilizador' => 1,
+            'it_tipo_utilizador' => 0,
         ];
     }
 
